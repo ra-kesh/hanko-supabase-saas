f12d612e67e054c222cb84d168427ae8075125f9
ra-kesh <iamrakeshkumar@pm.me>
2023-10-20 02:39:07 +0530

    refactor(editor):added both post content and title

--- components/editor/Editor.component.tsx
+++ components/editor/Editor.component.tsx
@@ -1,14 +1,18 @@
-import useLocalStorage from "@/lib/useLocalStorage";
-import { EditorContent, JSONContent, useEditor } from "@tiptap/react";
+"use client";
+
+import { EditorContent, useEditor } from "@tiptap/react";
 import React, { useEffect, useState } from "react";
 import { useDebouncedCallback } from "use-debounce";
 import { TiptapEditorProps } from "./props";
 import { TiptapExtensions } from "./extension";
+import { useRouter } from "next/navigation";
 
-const Editor = ({ content, setContent }) => {
-  // const [content, setContent] = useLocalStorage("content", []);
+const Editor = ({ post }) => {
+  const [title, setTitle] = useState(post.title);
   const [saveStatus, setSaveStatus] = useState("saved");
   const [hydrated, setHydrated] = useState(false);
+
+  const router = useRouter();
 
   const editor = useEditor({
     extensions: TiptapExtensions,
@@ -19,25 +23,64 @@ const Editor = ({ content, setContent }) => {
     },
   });
 
+  async function updatePostConent(content: string) {
+    setSaveStatus("saving...");
+
+    const response = await fetch(`/api/post/${post.postId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        content,
+      }),
+    });
+
+    setSaveStatus("saved");
+
+    router.refresh();
+  }
+
   const debouncedUpdates = useDebouncedCallback(async ({ editor }) => {
     const json = editor?.getJSON();
-    setSaveStatus("saving...");
-    setContent(json);
-    setTimeout(() => {
-      setSaveStatus("saved");
-    }, 500);
+    try {
+      await updatePostConent(json);
+    } catch (error) {
+      console.log(error);
+    }
   }, 700);
 
+  // useEffect(() => {
+  //   const saveInterval = setInterval(() => {
+  //     debouncedUpdates({ editor });
+  //   }, 1 * 60 * 1000);
+
+  //   return () => {
+  //     clearInterval(saveInterval);
+  //   };
+  // }, [debouncedUpdates]);
+
   useEffect(() => {
-    if (editor && content && !hydrated) {
-      editor.commands.setContent(content);
+    if (editor && post && !hydrated) {
+      editor.commands.setContent(JSON.parse(post.content));
       editor.commands.focus("end");
       setHydrated(true);
     }
-  }, [editor, content, hydrated]);
+  }, [editor, post, hydrated]);
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTitle(event.target.value);
+  };
 
   return (
-    <div className="relative min-h-[500px] w-full max-w-screen-lg border-gray-200 p-12 sm:rounded-lg sm:border sm:shadow-lg">
+    <div className="relative min-h-[500px] w-full max-w-screen-lg  p-12">
+      <textarea
+        value={title}
+        onChange={handleTitleChange}
+        className="w-full rounded-md mb-2 focus:outline-none"
+        placeholder="Enter the title"
+      />
       <EditorContent editor={editor} />
       <div className="absolute right-5 bottom-5 rounded-lg bg-gray-100 px-2 py-1 text-sm text-gray-400">
         {saveStatus}
