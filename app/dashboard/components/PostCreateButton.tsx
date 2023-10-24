b6c442a8ace1aabb97a48a79195394ca98ed2f9d
ra-kesh <iamrakeshkumar@pm.me>
2023-10-24 12:17:09 +0530

    refactor(post):post creation toast

--- app/dashboard/components/PostCreateButton.tsx
+++ app/dashboard/components/PostCreateButton.tsx
@@ -1,68 +1,39 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import React, { ReactNode, useState } from "react";
+import React from "react";
 import { toast } from "sonner";
-
-type Post = {
-  postId: string;
-  title: string;
-  content: string | null;
-  published: boolean;
-  createdAt: Date;
-  updatedAt: Date;
-  authorId: string;
-};
 
 const PostCreateButton = () => {
   const router = useRouter();
 
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  async function createPost(): Promise<void> {
+    const response = await fetch("api/post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: "Title",
+      }),
+    });
 
-  async function createPost(): Promise<Post> {
-    setIsLoading(true);
-
-    try {
-      const response = await fetch("api/post", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          title: "Untitled",
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error("Failed to create a new post.");
-      }
-
-      const { post } = await response.json();
-      return post;
-    } catch (error) {
-      throw error;
-    } finally {
-      setIsLoading(false);
+    if (!response.ok) {
+      throw new Error("Failed to create a new post.");
     }
-  }
 
-  function handlePostCreation(post: Post | null): ReactNode {
+    const { post } = await response.json();
+
+    router.refresh();
+
     router.push(`/editor/${post?.postId}`);
-    toast.success("New post created!");
-    return null;
   }
 
-  function handlePostCreationError(error: Error): ReactNode {
-    toast.error("Failed to create a new post.");
-    console.error(error);
-    return null;
-  }
-
-  async function onClickCreatePost() {
-    toast.promise<Post>(createPost(), {
+  function onClickCreatePost() {
+    toast.promise<void>(createPost(), {
       loading: "Creating a new post...",
-      success: handlePostCreation,
-      error: handlePostCreationError,
+      success: "New post created!",
+      error: "Failed to create a new post.",
     });
   }
 
