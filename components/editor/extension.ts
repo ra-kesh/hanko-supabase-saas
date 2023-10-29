65b6150d80070fe86e08db2bdabd17e359901179
ra-kesh <iamrakeshkumar@pm.me>
2023-10-29 23:32:58 +0530

    style(edior):custom styles for tiptap starter kit

--- components/editor/extension.ts
+++ components/editor/extension.ts
@@ -3,9 +3,36 @@ import Placeholder from "@tiptap/extension-placeholder";
 import { SlashCommand } from "./SlashCommand";
 
 export const TiptapExtensions = [
-  StarterKit,
+  StarterKit.configure({
+    bulletList: {
+      HTMLAttributes: {
+        class: "list-disc list-outside leading-3",
+      },
+    },
+    orderedList: {
+      HTMLAttributes: {
+        class: "list-decimal list-outside leading-3",
+      },
+    },
+    blockquote: {
+      HTMLAttributes: {
+        class: "border-l-4 border-gray-300 pl-4",
+      },
+    },
+    codeBlock: {
+      HTMLAttributes: {
+        class: "rounded-md bg-muted p-5 font-mono font-medium text-gray-800",
+      },
+    },
+    code: {
+      HTMLAttributes: {
+        class:
+          "rounded-md bg-muted px-1.5 py-1 font-mono font-medium text-black",
+      },
+    },
+  }),
   Placeholder.configure({
-    placeholder: "press '/' for commands",
+    placeholder: "use mdx or press '/' for commands",
   }),
   SlashCommand,
 ];
